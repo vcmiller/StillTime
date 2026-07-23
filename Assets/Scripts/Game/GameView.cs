dfbb227c2a7bf6b8cf17b49e47b4fbc1d1c5ca2e
Val Miller <infohazard.games@gmail.com>
2026-07-23 13:39:41 -0700

    UI improvements

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -13,11 +13,19 @@ namespace Game {
         public ChoiceView _choiceViewPrefab;
         public Transform _choiceViewParent;
 
+        public PassiveTimer _showWordTimer;
+        public PassiveTimer _showButtonTimer;
+
         private Action _mainButtonAction;
+        private int _visibleButtonCount;
+        private readonly List<ChoiceView> _currentChoices = new();
 
         private void OnEnable() {
             _mainButton.onClick.RemoveListener(HandleButton);
             _mainButton.onClick.AddListener(HandleButton);
+            
+            _showWordTimer.Initialize();
+            _showButtonTimer.Initialize();
         }
 
         private void OnDisable() {
@@ -25,24 +33,66 @@ namespace Game {
         }
 
         private void HandleButton() {
-            _mainButtonAction?.Invoke();
+            if (_mainText.maxVisibleWords < _mainText.textInfo.wordCount ||
+                _visibleButtonCount < _currentChoices.Count) {
+                _mainText.maxVisibleWords = _mainText.textInfo.wordCount;
+                for (int i = 0; i < _currentChoices.Count; i++) {
+                    _currentChoices[i].transform.localScale = Vector3.one;
+                }
+
+                _visibleButtonCount = _currentChoices.Count;
+                _mainButton.gameObject.SetActive(_currentChoices.Count == 0);
+            } else {
+                _mainButtonAction?.Invoke();
+            }
         }
 
         public void SetSingleText(string text, Action next) {
             Clear();
 
             _mainText.text = text;
+            _showWordTimer.StartInterval();
             _mainButtonAction = next;
-            _mainButton.gameObject.SetActive(true);
         }
 
         public void SetChoices(string mainText, List<(string text, Action action)> choices) {
             Clear();
 
             _mainText.text = mainText;
+            _showWordTimer.StartInterval();
             foreach ((string text, Action action) in choices) {
                 ChoiceView choiceView = Instantiate(_choiceViewPrefab, _choiceViewParent, false);
                 choiceView.Configure(text, action);
+                choiceView.transform.localScale = Vector3.zero;
+                _currentChoices.Add(choiceView);
+            }
+        }
+
+        private void Update() {
+            if (_mainText.isActiveAndEnabled &&
+                _mainText.text.Length > 0 && 
+                _mainText.maxVisibleWords < _mainText.textInfo.wordCount &&
+                _showWordTimer.TryConsume()) {
+                _mainText.maxVisibleWords++;
+                _showButtonTimer.StartInterval();
+            }
+
+            if (_mainText.maxVisibleWords >= _mainText.textInfo.wordCount &&
+                _currentChoices.Count > 0 &&
+                _visibleButtonCount < _currentChoices.Count) {
+
+                ChoiceView currentButton = _currentChoices[_visibleButtonCount];
+                float ratio = _showButtonTimer.RatioSinceIntervalStart;
+                currentButton.transform.localScale = Vector3.one * ratio;
+
+                if (ratio == 1) {
+                    _showButtonTimer.StartInterval();
+                    _visibleButtonCount++;
+
+                    if (_visibleButtonCount == _currentChoices.Count) {
+                        _mainButton.gameObject.SetActive(false);
+                    }
+                }
             }
         }
 
@@ -50,7 +100,10 @@ namespace Game {
             _choiceViewParent.DestroyChildren();
             _mainButtonAction = null;
             _mainText.text = string.Empty;
-            _mainButton.gameObject.SetActive(false);
+            _mainText.maxVisibleWords = 0;
+            _mainButton.gameObject.SetActive(true);
+            _visibleButtonCount = 0;
+            _currentChoices.Clear();
         }
     }
 }
